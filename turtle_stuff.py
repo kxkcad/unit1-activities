97ef36716658057302d2768d5b9ec28e8207b667
kxkcad <89785581+kxkcad@users.noreply.github.com>
2021-09-08 23:22:17 +0400

    turtle example

---
+++ turtle_stuff.py
@@ -0,0 +1,18 @@
+import turtle
+
+
+def test_drive():
+   turtle.forward(100)
+   #turtle.left(87)
+   turtle.setheading(127)
+   turtle.up() 
+   turtle.goto(50, 50)
+   turtle.down()
+   turtle.home()
+   turtle.circle(50)
+
+
+def main():
+   test_drive()
+
+main()
